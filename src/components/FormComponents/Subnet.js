a9fa968d06d3d041bc5de67829c9666fdc32e4e7
Aravindh036 <aravindh036@gmail.com>
2018-12-30 21:57:37 +0530

    temp

--- src/components/FormComponents/Subnet.js
+++ src/components/FormComponents/Subnet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './FormComponents.css';
 export default class Subnet extends Component {
   state={
+    name:"",
     CidrBlock:"",
     SubnetType:false,
     VpcId:""
@@ -21,6 +22,11 @@ export default class Subnet extends Component {
       VpcId:e.target.value
     });
   }
+  getName=(e)=>{
+    this.setState({
+      name:e.target.value
+    });
+  }
   saveForm = () => {
     console.log("hello");
     if ((this.state.CidrBlock !== "") && (this.state.VpcId !== "")) {
@@ -38,6 +44,7 @@ export default class Subnet extends Component {
       document.getElementById("private-subnet-form-id").classList.toggle("hide");
       document.getElementById("cidr-block-id").value = "";
       document.getElementById("subnet-vpc-id").value = "";
+      document.getElementById("subnet-name-id").value = "";
     }
   }
   render() {
@@ -46,6 +53,7 @@ export default class Subnet extends Component {
     return (
       <div className="ec2-form hide" id="private-subnet-form-id">
         <div className="form-elements">
+            <input type="text" placeholder="SubnetName" id="subnet-name-id" onBlur={this.getName} />
             <input onBlur={this.getCidrBlock} type="text" placeholder="CidrBlock" id="cidr-block-id"/>
             <select onChange={this.getSubnetType} name="SubnetType" id="subnet-type-id">
               <option value="false">Public</option>
