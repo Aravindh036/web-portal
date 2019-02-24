476210ddf562fe8fc0ccc579a0c1d40438efe152
aravindh balaji <aravindh036@gmail.com>
2019-02-24 15:17:23 +0530

    login and signup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Editor from './components/Editor/Editor';
+import Login from './components/Login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Editor/>
+        <Login/>
       </div>
     );
   }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+import './Login.css';
+
+import logo from '../../assets/logo.png';
+
+class Login extends Component {
+    state = {
+        login: true,
+        signup: false
+    };
+    getLogin = () => {
+        if (this.state.login !== true) {
+            this.setState({
+                login: true,
+                signup: false
+            });
+            document.getElementById("form-line-id").classList.toggle("move-line-right");
+        }
+    }
+    getSignup = () => {
+        if (this.state.signup !== true) {
+            this.setState({
+                login: false,
+                signup: true
+            });
+            document.getElementById("form-line-id").classList.toggle("move-line-right");
+        }
+    }
+    render() {
+        return (
+            <div className="home-container" >
+                <div className="home-image">
+                    <img src={logo} alt="su"/>
+                    <div className="form-container">
+                        <div className="login-signup">
+                            <div className="login" onClick={this.getLogin} >Login</div>
+                            <div className="signup" onClick={this.getSignup} >Sign up</div>
+                            <div id="form-line-id" className="under-line-form"></div>
+                        </div>
+                        <div className="login-signup-form">
+                            {this.state.login === true ?
+                                <div className="login-form">
+                                    <div className="align-start">
+                                        <label>Email</label>
+                                        <input type="text" />
+                                    </div>
+                                    <div className="align-start">
+                                        <label>Password</label>
+                                        <input type="password" />
+                                    </div>
+                                    <button className="form-button">Login</button>
+                                </div> : null}
+                            {this.state.signup === true ?
+                                <div className="signup-form">
+                                    <div className="align-start">
+                                        <label>Email</label>
+                                        <input type="text" />
+                                    </div>
+                                    <div className="align-start">
+                                        <label>Password</label>
+                                        <input type="password" />
+                                    </div>
+                                    <button className="form-button">Signup</button>
+                                </div> : null}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
