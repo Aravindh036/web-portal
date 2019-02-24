50e7e551d7dbfd20d69671aaad6e1e2d9e3055f8
aravindh balaji <aravindh036@gmail.com>
2019-02-24 15:44:28 +0530

    background added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -31,38 +31,40 @@ class Login extends Component {
         return (
             <div className="home-container" >
                 <div className="home-image">
-                    <img src={logo} alt="su"/>
-                    <div className="form-container">
-                        <div className="login-signup">
-                            <div className="login" onClick={this.getLogin} >Login</div>
-                            <div className="signup" onClick={this.getSignup} >Sign up</div>
-                            <div id="form-line-id" className="under-line-form"></div>
-                        </div>
-                        <div className="login-signup-form">
-                            {this.state.login === true ?
-                                <div className="login-form">
-                                    <div className="align-start">
-                                        <label>Email</label>
-                                        <input type="text" />
-                                    </div>
-                                    <div className="align-start">
-                                        <label>Password</label>
-                                        <input type="password" />
-                                    </div>
-                                    <button className="form-button">Login</button>
-                                </div> : null}
-                            {this.state.signup === true ?
-                                <div className="signup-form">
-                                    <div className="align-start">
-                                        <label>Email</label>
-                                        <input type="text" />
-                                    </div>
-                                    <div className="align-start">
-                                        <label>Password</label>
-                                        <input type="password" />
-                                    </div>
-                                    <button className="form-button">Signup</button>
-                                </div> : null}
+                    <div className="container">
+                        <img src={logo} alt="su" />
+                        <div className="form-container">
+                            <div className="login-signup">
+                                <div className="login" onClick={this.getLogin} >Login</div>
+                                <div className="signup" onClick={this.getSignup} >Sign up</div>
+                                <div id="form-line-id" className="under-line-form"></div>
+                            </div>
+                            <div className="login-signup-form">
+                                {this.state.login === true ?
+                                    <div className="login-form">
+                                        <div className="align-start">
+                                            <label>Email</label>
+                                            <input type="text" />
+                                        </div>
+                                        <div className="align-start">
+                                            <label>Password</label>
+                                            <input type="password" />
+                                        </div>
+                                        <button className="form-button">Login</button>
+                                    </div> : null}
+                                {this.state.signup === true ?
+                                    <div className="signup-form">
+                                        <div className="align-start">
+                                            <label>Email</label>
+                                            <input type="text" />
+                                        </div>
+                                        <div className="align-start">
+                                            <label>Password</label>
+                                            <input type="password" />
+                                        </div>
+                                        <button className="form-button">Signup</button>
+                                    </div> : null}
+                            </div>
                         </div>
                     </div>
                 </div>
